4abc3883fdeb25fefcb28d4c1152a0b7f1129cd2
Roshni <627032@student.inholland.nl>
2022-12-10 00:27:17 +0100

    added models

--- src/exploring.py
+++ src/exploring.py
@@ -4,7 +4,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 
 #Loading data
 file_errors_location = "D:\\University\\FourthYear\\SecondTerm\\DataScience\\Data\\SelfAssessmentAndTestCenter.xlsx"
@@ -29,18 +35,26 @@ print(df[0:5])
 #dropping unused data
 df.drop(['X108_04'], axis=1, inplace=True)
 df= df.dropna()
-'''
-#correlations
-print("\nCORRELATIONS:")
-corr=(df[3:].corr())
-print(corr)
-sns.heatmap(df[3:].corr(),annot=True)
-plt.show()'''
+
+# Calculating BMI from weight and height
+# Formula : ( weight (kg) / height (cm) / height (cm) )x 10,000
+df['bmi'] = ((df['X109_08']/df['X109_07']/df['X109_07'])*10000).round(2)
+
+# Some rows for both weight and height have the value 0
+df.loc[df['X109_07'] == 0]
+df.loc[df['X109_08'] == 0]
+
+# That results in some rows in bmi having the values 0 or inf
+print("bmi",df['bmi'])
+
+# We remove rows with the value 'inf' or 0 from bmi
+df= df[df['bmi'] != float("inf")]
+df = df[df['bmi'] != 0]
 
 #select columns with index positions 1 and 3
 a = df.iloc[:,[1,3]]
 print(a)
-b = df.iloc[:,6:]
+b = df.iloc[:,5:]
 print(b)
 
 updated_data =pd.concat([a,b],axis=1)
@@ -48,23 +62,116 @@ print(updated_data)
 
 #splitting target from data
 X = updated_data
-Y = df.iloc[:,5]
+Y = df.iloc[:,4]
 
+# Standardizing the features, because bmi is in higher range compared to likert scale 
+X= StandardScaler().fit_transform(X)
+print("After standardizing",X)
+
+# Creating a PCA object with 0.99 as the target explained variance
+pca = PCA(n_components=0.99)
+
+# Fiting the PCA model to the data
+pca.fit(X)
+
+# Transforming the data using the fitted PCA model
+X_pca = pca.transform(X)
+
+# Creating a new dataframe from the transformed data
+column_names = ['PCA_' + str(i+1) for i in range(X_pca.shape[1])]
+df_pca = pd.DataFrame(data=X_pca, columns=column_names)
+print(df_pca)
 
 #splitting data into train, valid and test data
-train_data, test_data, train_label, test_label = train_test_split(X, Y, test_size=0.1, random_state=1)
+train_data, test_data, train_label, test_label = train_test_split(df_pca, Y, test_size=0.05, random_state=1)
 train_data, valid_data, train_label, valid_label = train_test_split(train_data, train_label, test_size=0.15, random_state=1)
 
-#k-nearest neighbor model
-# build a k-NN model with k=3, k nearest model
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(train_data, train_label)
+def logistic_regression():
+    # Train the logistic regression model
+    model = LogisticRegression(max_iter=3000)
+    model.fit(train_data, train_label)
 
-# evaluate predictions
-train_accuracy = knn.score(train_data, train_label)
-print('Accuracy: %.2f' % (train_accuracy*100))
+    # Make predictions on new data
+    y_pred = model.predict(test_data)
 
-                              
-test_accuracy = knn.score(test_data, test_label)
-print('Accuracy: %.2f' % (test_accuracy*100))
+    # Calculate the accuracy of the model
+    accuracy = accuracy_score(test_label, y_pred)
+    print(accuracy)
+
+def knn_model():
+    # Setup arrays to store train and test accuracies
+    neighbors = np.arange(1, 9)
+    train_accuracy = np.empty(len(neighbors))
+    test_accuracy = np.empty(len(neighbors))
+
+    # Loop over different values of k
+    for i, k in enumerate(neighbors):
+        # Setup a k-NN Classifier with k neighbors: knn
+        knn = KNeighborsClassifier(n_neighbors=k)
+
+        # Fit the classifier to the training data
+        knn.fit(train_data, train_label)
+        
+        #Compute accuracy on the training set
+        train_accuracy[i] = knn.score(train_data, train_label)
+
+        #Compute accuracy on the testing set
+        test_accuracy[i] = knn.score(test_data, test_label)
+
+    # Generate plot
+    plt.title('k-NN: Varying Number of Neighbors')
+    plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+    plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+    plt.legend()
+    plt.xlabel('Number of Neighbors')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+def naive_bayes():
+    nb = GaussianNB()
+
+    # Training the model
+    nb.fit(train_data, train_label)
+
+    # Make predictions on new data
+    y_pred = nb.predict(test_data)
+
+    # Calculate the accuracy of the model
+    accuracy = accuracy_score(test_label, y_pred)
+    print(accuracy)
+
+def svm_model():
+    svm = SVC()
+
+    # Training the model
+    svm.fit(train_data, train_label)
+
+    # Make predictions on new data
+    y_pred = svm.predict(test_data)
+
+    # Calculate the accuracy of the model
+    accuracy = accuracy_score(test_label, y_pred)
+    print(accuracy)
+
+def decision_tree():
+    dt = DecisionTreeClassifier()
+
+    # Training the model
+    dt.fit(train_data, train_label)
+
+    # Make predictions on new data
+    y_pred = dt.predict(test_data)
+
+    # Calculate the accuracy of the model
+    accuracy = accuracy_score(test_label, y_pred)
+    print('Test Accuracy: %.2f' % (accuracy*100))
+
+logistic_regression()
+naive_bayes()
+svm_model()
+decision_tree()
+    
+
+
+
 
