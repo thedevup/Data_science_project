1203b3fdf9431748ed8da3db14259677e51289bf
Roshni <627032@student.inholland.nl>
2022-12-22 12:55:35 +0100

    exploring new dataset and getting feature importance

--- src/exploring_biggerDataset.py
+++ src/exploring_biggerDataset.py
@@ -15,59 +15,13 @@ from sklearn import metrics
 from sklearn.inspection import permutation_importance
 
 #Loading data
-file_errors_location = "D:\\University\\FourthYear\\SecondTerm\\DataScience\\Data\\SelfAssessmentAndTestCenter.xlsx"
+file_errors_location = "D:\\University\\FourthYear\\SecondTerm\\DataScience\\Data\\TS2018-2019_AISS (AM students).xlsx"
 df = pd.read_excel(file_errors_location)
-#replacing 0 with nan
-df['X108_03'] = df['X108_03'].replace(0, np.nan)
-df['X108_04'] = df['X108_04'].replace(0, np.nan)
-df['X109_07'] = df['X109_07'].replace(0, np.nan)
-df['X109_08'] = df['X109_08'].replace(0, np.nan)
-df=df.fillna(df.mean())
-print(df)
-
-#changing to likert scale
-conditions = [
-    (df['X108_04'] <=20),
-    ((df['X108_04'] > 20) & (df['X108_04'] <=40) ),
-    ((df['X108_04'] > 40) & (df['X108_04'] <=60)),
-    ((df['X108_04'] > 60) & (df['X108_04'] <=80)),
-    (df['X108_04'] > 80)
-    ]
-
-# create a list of the values we want to assign for each shooting power
-values = [1,2,3,4,5]
-
-# create a new column and use np.select to assign values to it using our lists as arguments
-df['shooting_power'] = np.select(conditions, values)
-print(df[0:5])
-
-#dropping unused data
-df.drop(['X108_04'], axis=1, inplace=True)
-
-# Calculating BMI from weight and height
-# Formula : ( weight (kg) / height (cm) / height (cm) )x 10,000
-df['bmi'] = ((df['X109_08']/df['X109_07']/df['X109_07'])*10000).round(2)
-
-# Some rows for both weight and height have the value 0
-df.loc[df['X109_07'] == 0]
-df.loc[df['X109_08'] == 0]
-
-
-# That results in some rows in bmi having the values 0 or inf
-print("bmi",df['bmi'])
-
-# We remove rows with the value 'inf' or 0 from bmi
-df= df[df['bmi'] != float("inf")]
-df = df[df['bmi'] != 0]
-
-df.drop(['X109_07'], axis=1, inplace=True)
-df.drop(['X109_08'], axis=1, inplace=True)
-
+df.info()
+    
 #select columns with index positions 1 and 3
 a = df.iloc[:,[1,3]]
-print(a)
 b = df.iloc[:,6:]
-print(b)
 
 updated_data =pd.concat([a,b],axis=1)
 print(updated_data)
@@ -77,20 +31,42 @@ X = updated_data.values
 Y = df.iloc[:,5]
 
 X_col = updated_data
+print(X)
+print(Y)
 
-# Standardizing the features, because bmi is in higher range compared to likert scale 
-X= MinMaxScaler().fit_transform(X)
-print("After standardizing",X)
+# Normalize Features
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
 
 # Creating a PCA object with 0.99 as the target explained variance
-pca = PCA(n_components=0.99)
+pca = PCA(n_components=0.99).fit(X)
 
 # Fiting the PCA model to the data
-pca.fit(X)
+#pca.fit(X)
 
 # Transforming the data using the fitted PCA model
 X_pca = pca.transform(X)
+#------
 
+# number of components
+n_pca= pca.components_.shape[0]
+
+# get the index of the most important feature on EACH component
+# LIST COMPREHENSION HERE
+most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_pca)]
+
+initial_feature_names = X_col.columns
+# get the names
+most_important_names = [initial_feature_names[most_important[i]] for i in range(n_pca)]
+
+# LIST COMPREHENSION HERE AGAIN
+dic = {'PCA{}'.format(i+1): most_important_names[i] for i in range(n_pca)}
+
+# build the dataframe
+df = pd.DataFrame(dic.items())
+print(df)
+
+#-----
 # Creating a new dataframe from the transformed data
 column_names = ['PCA_' + str(i+1) for i in range(X_pca.shape[1])]
 df_pca = pd.DataFrame(data=X_pca, columns=column_names)
@@ -105,6 +81,15 @@ def logistic_regression():
     model = LogisticRegression(max_iter=3000)
     model.fit(train_data, train_label)
 
+    # get importance
+    importance = model.coef_[0]
+    # summarize feature importance
+    for i,v in enumerate(importance):
+     print("feature:", df_pca.columns[i],"Importance score: ",format(v, '.5f'))
+    # plot feature importance
+    plt.bar([x for x in range(len(importance))], importance)
+    plt.show()
+
     # Make predictions on new data
     y_pred = model.predict(test_data)
 
@@ -114,39 +99,12 @@ def logistic_regression():
     print(metrics.classification_report(test_label,y_pred))
 
 def knn_model():
-    '''# Setup arrays to store train and test accuracies
-    neighbors = np.arange(1, 9)
-    train_accuracy = np.empty(len(neighbors))
-    test_accuracy = np.empty(len(neighbors))
-
-    # Loop over different values of k
-    for i, k in enumerate(neighbors):
-        # Setup a k-NN Classifier with k neighbors: knn
-        knn = KNeighborsClassifier(n_neighbors=k)
-
-        # Fit the classifier to the training data
-        knn.fit(train_data, train_label)
-        
-        #Compute accuracy on the training set
-        train_accuracy[i] = knn.score(train_data, train_label)
-
-        #Compute accuracy on the testing set
-        test_accuracy[i] = knn.score(test_data, test_label)
-
-    # Generate plot
-    plt.title('k-NN: Varying Number of Neighbors')
-    plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
-    plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
-    plt.legend()
-    plt.xlabel('Number of Neighbors')
-    plt.ylabel('Accuracy')
-    plt.show()'''
     neigh = KNeighborsClassifier(n_neighbors=7)
 
 
     # Training the model
     neigh.fit(train_data, train_label)
-
+   
     # perform permutation importance
     results = permutation_importance(neigh,train_data, train_label, scoring='accuracy')
     # get importance
@@ -154,7 +112,7 @@ def knn_model():
     # summarize feature importance
     for i,v in enumerate(importance):
        #print('Feature: %0d, Score: %.5f' % (i,v))
-       print("feature:", X_col.columns[i],"Importance score: ",format(v, '.5f'))
+       print("feature:", df_pca.columns[i],"Importance score: ",format(v, '.5f'))
     # plot feature importance
     plt.bar([x for x in range(len(importance))], importance)
     plt.show()
@@ -173,6 +131,17 @@ def naive_bayes():
     # Training the model
     nb.fit(train_data, train_label)
 
+    # perform permutation importance
+    results = permutation_importance(nb,train_data, train_label, scoring='accuracy')
+    # get importance
+    importance = results.importances_mean
+    # summarize feature importance
+    for i,v in enumerate(importance):
+       print("feature:", df_pca.columns[i],"Importance score: ",format(v, '.5f'))
+    # plot feature importance
+    plt.bar([x for x in range(len(importance))], importance)
+    plt.show()
+
     # Make predictions on new data
     y_pred = nb.predict(test_data)
 
@@ -188,6 +157,15 @@ def svm_model():
     # Training the model
     svm.fit(train_data, train_label)
 
+    # get importance
+    importance = svm.coef_[0]
+    # summarize feature importance
+    for i,v in enumerate(importance):
+     print("feature:", df_pca.columns[i],"Importance score: ",format(v, '.5f'))
+    # plot feature importance
+    plt.bar([x for x in range(len(importance))], importance)
+    plt.show()
+
     # Make predictions on new data
     y_pred = svm.predict(test_data)
 
@@ -202,6 +180,15 @@ def decision_tree():
     # Training the model
     dt.fit(train_data, train_label)
 
+    # get importance
+    importance = dt.feature_importances_
+    # summarize feature importance
+    for i,v in enumerate(importance):
+       print("feature:", df_pca.columns[i],"Importance score: ",format(v, '.5f'))
+    # plot feature importance
+    pyplot.bar([x for x in range(len(importance))], importance)
+    pyplot.show()
+
     # Make predictions on new data
     y_pred = dt.predict(test_data)
 
@@ -210,14 +197,8 @@ def decision_tree():
     print('Decision_Tree accuracy: %.2f' % (accuracy*100))
     print(metrics.classification_report(test_label,y_pred))
 
-'''logistic_regression()
+#logistic_regression()
 naive_bayes()
-svm_model()
-decision_tree()'''
-knn_model()
-
-    
-
-
-
-
+'''svm_model()
+decision_tree()
+knn_model()'''
